f3960dc1e83ee709b0e3aeb87b245f67e6492af2
Martin Fitzpatrick <mfitzp@abl.es>
2014-02-12 14:23:47 +0000

    Add installation information to developer docs

--- pathomx/threads.py
+++ pathomx/threads.py
@@ -35,6 +35,13 @@ class Worker(QRunnable):
     Worker thread
     
     Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread. Supplied args and 
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+
     '''          
     def __init__(self, callback, *args, **kwargs):
         super(Worker, self).__init__()
