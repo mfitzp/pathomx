6e1a9eeb0d829b8632c0e3eeaf7bea09f4e9e7ae
Martin Fitzpatrick <mfitzp@abl.es>
2014-04-01 16:53:32 +0100

    Implement Qt5/Qt4 wrapper to attempt to support Qt4 (requires testing).

--- pathomx/qt.py
+++ pathomx/qt.py
@@ -1,12 +1,47 @@
 from __future__ import unicode_literals
+import sys
 
-# Import PyQt5 classes accessible in elsewhere through
-# import qt5
-# qt5.QObject etc.
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWebKit import *
-from PyQt5.QtNetwork import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebKitWidgets import *
-from PyQt5.QtPrintSupport import *
+USE_PYQT = None
+
+if 'PyQt5' in sys.modules:
+    USE_PYQT = 5
+elif 'PyQt4' in sys.modules:
+    USE_PYQT = 4
+else:
+    try:
+        import PyQt5
+        USE_PYQT = 5
+    except ImportError:
+        try:
+            import PyQt4
+            USE_PYQT = 4
+        except ImportError:
+            USE_PYQT = None
+        
+if USE_PYQT == 5:
+
+    # Import PyQt5 classes accessible in elsewhere through
+    # from qt import *
+    from PyQt5.QtGui import *
+    from PyQt5.QtCore import *
+    from PyQt5.QtWebKit import *
+    from PyQt5.QtNetwork import *
+    from PyQt5.QtWidgets import *
+    from PyQt5.QtWebKitWidgets import *
+    from PyQt5.QtPrintSupport import *
+
+elif USE_PYQT == 4:
+
+    # Import PyQt4 classes accessible in elsewhere through
+    # from qt import *
+    from PyQt4.QtGui import *
+    from PyQt4.QtCore import *
+    from PyQt4.QtWebKit import *
+    from PyQt4.QtNetwork import *
+
+    
+    
+
+
+
+    
