ede63bd1d8ebb9e5b3eb706c6edb0d8a2a647b37
Martin Fitzpatrick <mfitzp@abl.es>
2014-07-06 00:24:30 +0100

    Documentation fixes

--- developer_install_mac.sh
+++ developer_install_mac.sh
@@ -1,7 +1,7 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 brew install python pyqt 
-pip install numpy scipy poster matplotlib scikit-learn dill pyqtconfig
+pip install numpy scipy pandas poster matplotlib scikit-learn dill pyqtconfig
 pip install ipython[all]
 
 #brew install graphviz
