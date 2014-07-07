a95b638f99b4837af326c76d4078a5546a5e88cb
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2014-07-07 23:25:58 +0100

    Fix Linux install script; now functional. TODO: Debian package

--- developer_install_linux.sh
+++ developer_install_linux.sh
@@ -1,2 +1,2 @@
-sudo apt-get install python-qt4 python-numpy python-scipy python-pandas python-yapsy python-poster python-matplotlib python-scikits-learn python-pip ipython ipython-notebook ipython-notebook-common
+sudo apt-get install python-qt4 python-numpy python-scipy python-pandas python-yapsy python-poster python-matplotlib python-scikits-learn python-pip ipython ipython-notebook ipython-notebook-common pandoc
 sudo pip install dill pyqtconfig mplstyler
--- developer_install_mac.sh
+++ developer_install_mac.sh
@@ -1,6 +1,6 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-brew install python pyqt 
+brew install python pyqt pandoc
 pip install numpy scipy pandas yapsy poster matplotlib scikit-learn dill pyqtconfig mplstyler
 pip install ipython[all]
 
