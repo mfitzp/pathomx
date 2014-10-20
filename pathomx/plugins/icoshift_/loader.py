f1bf5620b3138d53d20c6a7d2c5ef0ebb3c7cc64
Martin Fitzpatrick <mfitzp@abl.es>
2014-10-20 12:00:20 +0200

    Improved icoshift tool with new implemented options

--- pathomx/plugins/icoshift_/loader.py
+++ pathomx/plugins/icoshift_/loader.py
@@ -12,6 +12,13 @@ from pathomx.plugins import ProcessingPlugin
 from pathomx.data import DataDefinition
 from pathomx.qt import *
 
+from collections import defaultdict
+
+'''
+def icoshift(xt,  xp,  inter='whole',  n='f', scale=None, coshift_preprocessing=False,
+             coshift_preprocessing_max_shift=None, fill_with_previous=True, average2_multiplier=3):
+
+'''
 
 # Dialog box for Metabohunter search options
 class IcoshiftConfigPanel(ui.ConfigPanel):
@@ -19,53 +26,99 @@ class IcoshiftConfigPanel(ui.ConfigPanel):
     def __init__(self, *args, **kwargs):
         super(IcoshiftConfigPanel, self).__init__(*args, **kwargs)
 
-        vw = QVBoxLayout()
+        self.display_options = defaultdict(dict)
+        self.config.updated.connect(self.change_display)
+
+        gb = QGroupBox('Target')
+        gd = QGridLayout()
+        gb.setLayout(gd)
         self.target_cb = QComboBox()
-        self.target_cb.addItems(['average', 'median', 'max', 'average2'])
+        self.target_cb.addItems(['average', 'median', 'max', 'average2', 'input_target'])
         self.config.add_handler('target', self.target_cb)
-        vw.addWidget(self.target_cb)
+        gd.addWidget(self.target_cb, 0, 0)
 
-        gb = QGroupBox('Algorithm')
-        gb.setLayout(vw)
+        average2_l = QLabel('Average2 multiplier')
+        average2_sb = QSpinBox()
+        gd.addWidget(average2_l, 1, 0)
+        gd.addWidget(average2_sb, 1, 1)
+        self.config.add_handler('average2_multiplier', average2_sb)
+        self.display_options['target']['average2'] = (average2_l, average2_sb)
         self.layout.addWidget(gb)
-
-        vw = QVBoxLayout()
-        self.mode_cb = QComboBox()
-        self.mode_cb.addItems(['whole', 'number_of_intervals', 'length_of_intervals', 'define', 'reference_signal'])
-        self.config.add_handler('alignment_mode', self.mode_cb)
-        vw.addWidget(self.mode_cb)
 
         gb = QGroupBox('Intervals')
-        gb.setLayout(vw)
-        self.layout.addWidget(gb)
+        gd = QGridLayout()
+        gb.setLayout(gd)
+        self.mode_cb = QComboBox()
 
-        vw = QVBoxLayout()
+        self.mode_cb.addItems(['whole', 'number_of_intervals', 'length_of_intervals'])  #, 'define', 'reference_signal'])
+        self.config.add_handler('intervals', self.mode_cb)
+        gd.addWidget(self.mode_cb, 0, 0)
+
+        number_int_l = QLabel('Number of intervals')
+        number_int_sb = QSpinBox()
+
+        self.config.add_handler('number_of_intervals', number_int_sb)
+        gd.addWidget(number_int_l, 1, 0)
+        gd.addWidget(number_int_sb, 1, 1)
+        self.display_options['intervals']['number_of_intervals'] = (number_int_l, number_int_sb)
+
+        length_intervals_l = QLabel('Length of intervals')
+        length_intervals_sb = QSpinBox()
+        self.config.add_handler('length_of_intervals', length_intervals_sb)
+        gd.addWidget(length_intervals_l, 2, 0)
+        gd.addWidget(length_intervals_sb, 2, 1)
+        self.layout.addWidget(gb)
+        self.display_options['intervals']['length_of_intervals'] = (length_intervals_l, length_intervals_sb)
+
+
+
+
+        gb = QGroupBox('Maximum shift')
+        gd = QGridLayout()
+        gb.setLayout(gd)
         self.mode_cb = QComboBox()
         self.mode_cb.addItems(['n', 'b', 'f'])
         self.config.add_handler('maximum_shift', self.mode_cb)
-        vw.addWidget(self.mode_cb)
+        gd.addWidget(self.mode_cb,0,0)
 
-        gb = QGroupBox('Maximum shift')
-        gb.setLayout(vw)
+        maxshift_l = QLabel('Max shift (n)')
+        maxshift_sb = QSpinBox()
+        self.config.add_handler('maximum_shift_n', maxshift_sb)
+        gd.addWidget(maxshift_l, 1, 0)
+        gd.addWidget(maxshift_sb, 1, 1)
         self.layout.addWidget(gb)
+        self.display_options['maximum_shift']['n'] = (maxshift_l, maxshift_sb)
 
-        vw = QVBoxLayout()
+        gb = QGroupBox('Co-shift preprocessing')
+        gd = QGridLayout()
+        gb.setLayout(gd)
 
         self.coshift_btn = QCheckBox('Enable co-shift preprocessing')
         #self.coshift_btn.setCheckable( True )
-        self.config.add_handler('coshift_proprocess', self.coshift_btn)
-        vw.addWidget(self.coshift_btn)
+        self.config.add_handler('coshift_preprocessing', self.coshift_btn)
+        gd.addWidget(self.coshift_btn, 0, 0)
 
-        self.mode_cb = QComboBox()
-        self.mode_cb.addItems(['n', 'b', 'f'])
-        self.config.add_handler('mode', self.mode_cb)
-        vw.addWidget(self.mode_cb)
+        self.coshift_max_cb = ui.QNoneDoubleSpinBox()
+        self.config.add_handler('coshift_preprocessing_max_shift', self.coshift_max_cb)
+        gd.addWidget(QLabel('Maximum shift'), 1, 0)
+        gd.addWidget(self.coshift_max_cb, 1, 1)
 
-        gb = QGroupBox('Co-shift preprocessing')
-        gb.setLayout(vw)
         self.layout.addWidget(gb)
 
+        self.change_display() # Set starting state
         self.finalise()
+
+    def change_display(self, *args):
+        for k, v in self.display_options.items():
+            ic = self.config.get(k)
+            for i, o in v.items():
+                for oo in o:
+                    if i == ic:
+                        oo.show()
+                    else:
+                        oo.hide()
+
+
 '''
 ALL OPTIONS
 [algorithm]
@@ -101,6 +154,8 @@ class IcoshiftApp(ui.IPythonApp):
         super(IcoshiftApp, self).__init__(*args, **kwargs)
 
         self.data.add_input('input_data')  # Add input slot
+        self.data.add_input('input_target')
+
         self.data.add_output('output_data')
 
         # Setup data consumer options
@@ -114,8 +169,13 @@ class IcoshiftApp(ui.IPythonApp):
 
         self.config.set_defaults({
             'target': 'average',
-            'alignment_mode': 'whole',
+            'intervals': 'whole',
             'maximum_shift': 'f',
+            'maximum_shift_n': 50,
+            'coshift_preprocessing': False,
+            'coshift_preprocessing_max_shift': None,
+            'average2_multiplier': 3,
+            'number_of_intervals': 50,
         })
 
         self.addConfigPanel(IcoshiftConfigPanel, 'Settings')
