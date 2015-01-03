e0135aaeed6db6de44a609d554998cd7c16c3b5f
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-01-03 17:37:05 +0000

    Linux developer install fixes

--- developer_install_linux.sh
+++ developer_install_linux.sh
@@ -1,3 +1,13 @@
+# We're using a development version of IPython so for now we need to get it via git
+cwd=$(pwd)
+mkdir /tmp/pathomx-install
+cd /tmp/pathomx-install
+git clone --recursive https://github.com/ipython/ipython.git
+cd ipython
+sudo pip install --upgrade .
+rm -rf /tmp/pathomx-install
+cd $cwd
+
+# Now install all other dependencies via apt-get or pip
 sudo apt-get install python-qt4 python-numpy python-scipy python-pandas python-yapsy python-requests python-matplotlib python-scikits-learn python-pip ipython ipython-notebook ipython-notebook-common ipython-qtconsole python-pygments
-# sudo apt-get install python-pkg-resources python-setuptools --reinstall
-sudo pip install --upgrade pyqtconfig mplstyler
+sudo pip install --upgrade pyqtconfig mplstyler mistune biocyc pydot
--- developer_install_linux_python3.sh
+++ developer_install_linux_python3.sh
@@ -1,4 +1,16 @@
+# We're using a development version of IPython so for now we need to get it via git
+cwd=$(pwd)
+mkdir /tmp/pathomx-install
+cd /tmp/pathomx-install
+git clone --recursive https://github.com/ipython/ipython.git
+cd ipython
+sudo pip3 install --upgrade .
+rm -rf /tmp/pathomx-install
+cd $cwd
+
+# Now install all other dependencies via apt-get or pip
 sudo apt-get install python3-pyqt4 python3-numpy python3-scipy python3-pandas python3-yapsy python3-matplotlib python3-requests python3-pip ipython3 ipython3-qtconsole ipython3-notebook
 sudo pip3 install --upgrade pyqtconfig mplstyler
+
 # The following may error, but is not essential
-sudo pip3 scikits.learn 
+sudo pip3 install scikits.learn 
